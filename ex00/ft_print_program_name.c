7cc297ecfebb041319b82c607cd9153278052e91
Alexandre Dubeau <adubeau@c2r4p5.42quebec.com>
2021-02-25 14:19:54 -0500

    ex00

--- ex00/ft_print_program_name.c
+++ ex00/ft_print_program_name.c
@@ -6,7 +6,7 @@
 /*   By: adubeau <marvin@42quebec.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/23 16:48:55 by adubeau           #+#    #+#             */
-/*   Updated: 2021/02/25 11:45:06 by adubeau          ###   ########.fr       */
+/*   Updated: 2021/02/25 14:19:34 by adubeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,6 @@ int	main(int argc, char **argv)
 {
 	(void)argc;
 	write(1, argv[0], ft_strlen(argv[0]));
+	write(1, "\n", 1);
 	return (0);
 }
